5f3034388acea71fd8f17c59f8dce35fcec960e3
fiddlydigit <alexandr.baranovschi@gmail.com>
2021-05-27 21:13:23 +0100

    commit

--- dist/main.1f19ae8e.js
+++ dist/main.1f19ae8e.js
@@ -86176,7 +86176,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50564" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50737" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
